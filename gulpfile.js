ba68be99a14473461832feaa423d580cd27ad229
Brian Nieves <brian@bitasy.me>
2018-10-20 03:17:43 -0400

    Update gulp to version 4, set up project for new laptop (add gitignore to VCS, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,58 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync').create();
+const { series, parallel, watch, src, dest } = require('gulp');
+var server  = require('browser-sync').create();
 var sass         = require('gulp-sass');
 var sassVarsToJs = require('gulp-sass-vars-to-js');
 var rename       = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var uncss        = require('gulp-uncss');
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src('src/scss/app.scss')
+function reload(done) {
+    server.reload();
+    done();
+}
+
+function serve(done) {
+    server.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    done();
+}
+
+
+function compile(){
+    return src('src/scss/app.scss')
         .pipe( sass() )
         .pipe( autoprefixer() )
         .pipe(uncss({
             html: ['index.html']
         }))
-        .pipe( gulp.dest('src/css/') )
-        .pipe( browserSync.stream() );
-});
+        .pipe( dest('src/css/') );
+}
 
-// Move the javascript files into our /src/js folder
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/js/popper.min.js'])
-        .pipe( gulp.dest("src/js") )
-        .pipe( browserSync.stream() );
-});
+function sassvars() {
+    return src(['src/scss/_customVariables.scss'])
+        .pipe( sassVarsToJs() )
+        .pipe( rename("vars.js") )
+        .pipe( dest("src/js") )
+}
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'sassvars'], function() {
+function safevars(done){
+    setTimeout(done, 1000);
+    return sassvars();
+}
 
-    browserSync.init({
-        server: "."
-    });
+function js(){
+    return src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js'])
+        .pipe( dest("src/js") );
+}
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass', 'sassvars']);
-    gulp.watch('*.html').on('change', browserSync.reload);
-    gulp.watch('src/js/app.js').on('change', browserSync.reload);
-});
+watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], series(compile, sassvars, reload));
+watch("*.html", reload);
+watch("src/js/*,js", reload);
 
-gulp.task('sassvars', function() {
-    gulp.src(['src/scss/_customVariables.scss'])
-        .pipe(sassVarsToJs())
-        .pipe(rename('vars.js'))
-        .pipe(gulp.dest('src/js'))
+watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], series(compile, sassvars, reload));
 
-})
-
-gulp.task('default', ['js','serve']);
+const dev = series(compile, safevars, js, serve, reload);
+exports.default = dev;
